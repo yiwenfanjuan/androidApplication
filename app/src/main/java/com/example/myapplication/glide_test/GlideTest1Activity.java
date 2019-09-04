0dd2503805ceb5a154e270d832144c22ec99fee9
张一凡 <yiwenfanjuan@163.com>
2019-09-04 17:51:41 +0800

    “ 继续学习Glide4.x源码 ”

--- app/src/main/java/com/example/myapplication/glide_test/GlideTest1Activity.java
+++ app/src/main/java/com/example/myapplication/glide_test/GlideTest1Activity.java
@@ -1,10 +1,13 @@
 package com.example.myapplication.glide_test;
 
 import android.Manifest;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.BaseRequestOptions;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.myapplication.BaseActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivityGlideTest1Binding;
@@ -26,6 +29,7 @@ public class GlideTest1Activity extends BaseActivity<ActivityGlideTest1Binding> 
     protected void initUi() {
 
         Glide.with(this)
+                .asFile()
                 .load("https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=92afee66fd36afc3110c39658318eb85/908fa0ec08fa513db777cf78376d55fbb3fbd9b3.jpg")
                 .into(mBinding.image);
 
